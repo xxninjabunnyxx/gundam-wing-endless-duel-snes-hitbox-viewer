69a44f9c584012ab6300c3070e954883346a5cff
Mason <xxninjabunnyxx@gmail.com>
2021-02-15 14:56:21 -0500

    found starter addressees and values

---
+++ main.lua
@@ -0,0 +1,19 @@
+memory.usememorydomain('WRAM')
+
+local time = { address = 0x00060C; max = 153 }
+
+local player_1 ={
+    health = { address = 0x001B71; max = 112};
+    meter = { address = 0x001B80; max = 300 }
+}
+
+local player_2 = {
+    health = { address = 0x001B75; max = 112};
+    meter = { address = 0x001B84; max = 300 }
+}
+
+
+while true do
+    memory.writebyte(time.address, time.max)
+    emu.frameadvance()
+end
