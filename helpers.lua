dde581b2bfc65358feac3199ba152ebde85066b3
Mason <xxninjabunnyxx@gmail.com>
2021-02-17 17:37:41 -0500

    added extra write condition

--- helpers.lua
+++ helpers.lua
@@ -2,9 +2,19 @@ local state = require("state")
 
 local module = {}
 
-function module.infinite(address, max)
-    function f()
-        memory.writebyte(address, max)
+function module.infinite(address, max, size)
+    if size == 1 then
+        function f()
+            memory.writebyte(address, max)
+        end
+    elseif size == 2 then
+        function f()
+            memory.write_u16_le(address, max)
+        end
+    else
+        function f()
+            memory.write_u16_le(address, max)
+        end
     end
     return f
 end
