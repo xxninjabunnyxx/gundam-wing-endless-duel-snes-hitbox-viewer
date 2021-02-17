16445eb2bed4dbf6d27dd7a5cb2e44c9c9336e71
Mason <xxninjabunnyxx@gmail.com>
2021-02-17 15:08:50 -0500

    refactored to have a global state file

---
+++ lib/state.lua
@@ -0,0 +1,26 @@
+local module = {}
+
+module.time = { address = 0x00060C; max = 153 }
+
+module.player_1 = {
+    health = { address = 0x001B71; max = 112};
+    meter = { address = 0x001B80; max = 300 }
+}
+
+module.player_2 = {
+    health = { address = 0x001B75; max = 112};
+    meter = { address = 0x001B84; max = 300 }
+}
+
+module.timers = {
+    main_overlay = 0;
+    menu_delay = 0;
+    sub_menu_delay = 0;
+}
+
+module.flags = {
+    main_overlay = false;
+    menu_state = 1;
+}
+
+return module
--- main.lua
+++ main.lua
@@ -1,33 +1,11 @@
 memory.usememorydomain('WRAM')
-
-local time = { address = 0x00060C; max = 153 }
-
-local player_1 = {
-    health = { address = 0x001B71; max = 112};
-    meter = { address = 0x001B80; max = 300 }
-}
-
-local player_2 = {
-    health = { address = 0x001B75; max = 112};
-    meter = { address = 0x001B84; max = 300 }
-}
-
-local timers = {
-    main_overlay = 0;
-    menu_delay = 0;
-    sub_menu_delay = 0;
-}
-
-local flags = {
-    main_overlay = false;
-    menu_state = 1;
-}
+local state = require("lib.state")
 
 function check_timers()
     ref_time = 50
-    if timers.main_overlay == ref_time then
-        flags.main_overlay = not flags.main_overlay
-        timers.main_overlay = 0
+    if state.timers.main_overlay == ref_time then
+        state.flags.main_overlay = not state.flags.main_overlay
+        state.timers.main_overlay = 0
     end
 end
 
@@ -106,22 +84,22 @@ function main_overlay()
             menu_text = value["text"]
         end
 
-        if key == flags.menu_state and value["skip"] == false then
+        if key == state.flags.menu_state and value["skip"] == false then
             gui.drawText(0, line_space, ">" .. menu_text, "white", "Black")
 
             if table_has_key(value, "state") == true and table_has_key(value, "max_state") == true then
-                if inputs["P1 Right"] == true and timers.sub_menu_delay >= ref_time then
+                if inputs["P1 Right"] == true and state.timers.sub_menu_delay >= ref_time then
                     value.state = value.state + 1
-                    timers.sub_menu_delay = 0
+                    state.timers.sub_menu_delay = 0
                 end
             
-                if inputs["P1 Left"] == true and timers.sub_menu_delay >= ref_time then
+                if inputs["P1 Left"] == true and state.timers.sub_menu_delay >= ref_time then
                     value.state = value.state - 1
-                    timers.sub_menu_delay = 0
+                    state.timers.sub_menu_delay = 0
                 end
     
                 if inputs["P1 Left"] == true or inputs["P1 Right"] == true then
-                    timers.sub_menu_delay = timers.sub_menu_delay + 1
+                    state.timers.sub_menu_delay = state.timers.sub_menu_delay + 1
                 end
     
                 if value.state > value.max_state then
@@ -132,7 +110,7 @@ function main_overlay()
                     value.state = 1
                 end
             end
-        elseif key == flags.menu_state and value["skip"] == true then
+        elseif key == state.flags.menu_state and value["skip"] == true then
             gui.drawText(0, line_space, "-" .. menu_text, "white", "Black")
         else
             gui.drawText(0, line_space, " " .. menu_text, "white", "Black")
@@ -142,25 +120,25 @@ function main_overlay()
     end
 
     if inputs["P1 Down"] == true or inputs["P1 Up"] == true then
-        timers.menu_delay = timers.menu_delay + 1
+        state.timers.menu_delay = state.timers.menu_delay + 1
     end
 
-    if inputs["P1 Down"] == true and timers.menu_delay >= ref_time then
-        flags.menu_state = flags.menu_state + 1
-        timers.menu_delay = 0
+    if inputs["P1 Down"] == true and state.timers.menu_delay >= ref_time then
+        state.flags.menu_state = state.flags.menu_state + 1
+        state.timers.menu_delay = 0
     end
 
-    if inputs["P1 Up"] == true and timers.menu_delay >= ref_time then
-        flags.menu_state = flags.menu_state - 1
-        timers.menu_delay = 0
+    if inputs["P1 Up"] == true and state.timers.menu_delay >= ref_time then
+        state.flags.menu_state = state.flags.menu_state - 1
+        state.timers.menu_delay = 0
     end
 
-    if flags.menu_state < 1 then
-        flags.menu_state = 1
+    if state.flags.menu_state < 1 then
+        state.flags.menu_state = 1
     end
 
-    if flags.menu_state > 13 then
-        flags.menu_state = 13
+    if state.flags.menu_state > 13 then
+        state.flags.menu_state = 13
     end
 end
 
@@ -168,15 +146,15 @@ while true do
     local inputs = joypad.get()
 
     if inputs["P1 Start"] == true and inputs["P1 Select"] == true then
-        timers.main_overlay = timers.main_overlay + 1
+        state.timers.main_overlay = state.timers.main_overlay + 1
     end
 
     check_timers()
 
-    if flags.main_overlay == true then
+    if state.flags.main_overlay == true then
         main_overlay()
     end
 
-    memory.writebyte(time.address, time.max)
+    memory.writebyte(state.time.address, state.time.max)
     emu.frameadvance()
 end
