6d44a3fa1c21f875b7dafdd0ab25de1f904cec5a
xxninjabunnyxx <xxninjabunnyxx@gmail.com>
2021-04-18 17:54:35 -0400

    added infinate time

--- main.lua
+++ main.lua
@@ -41,43 +41,50 @@ local state = {
     }
 }
 
+function one_byte_set_to_max(table)
+    function f()
+        memory.writebyte(table.address, table.max)
+    end
+    return f
+end
+
 local menu = {
-    [1] = { text = "Player 1"; skip = true };
-    [2] = { text = "Meter"; skip = false; state = 1; max_state = 3; options = {
-        [1] = {text = " Normal >";  callback = noop };
-        [2] = {text = "< Refill >";  callback = noop };
-        [3] = {text = "< Infinate";  callback = noop }
-    }};
-    [3] = { text = "Player 2"; skip = true };
-    [4] = { text = "Health"; skip = false; state = 1; max_state = 3; options = {
-        [1] = {text = " Normal >";  callback = noop };
-        [2] = {text = "< Refill >";  callback = noop };
-        [3] = {text = "< Infinate";  callback = noop }
-    }};
-    [5] = { text = "Meter"; skip = false; state = 1; max_state = 3; options = {
-        [1] = {text = " Normal >";  callback = noop };
-        [2] = {text = "< Refill >";  callback = noop };
-        [3] = {text = "< Infinate";  callback = noop }
-    }};
-    [6] = { text = "State"; skip = false; state = 1; max_state = 3; options = {
-        [1] = {text = " Standing >";  callback = noop };
-        [2] = {text = "< Crouching >";  callback = noop };
-        [3] = {text = "< Jumping";  callback = noop }
-    }};
-    [7] = { text = "Blocking"; skip = false; state = 1; max_state = 3; options = {
-        [1] = {text = " None >";  callback = noop };
-        [2] = {text = "< Auto >";  callback = noop };
-        [3] = {text = "< Follow Up";  callback = noop }
-    }};
-    [8] = { text = "Extras"; skip = true };
-    [9] = { text = "Time"; skip = false; state = 1; max_state = 2; options = {
+    --[1] = { text = "Player 1"; skip = true };
+    --[2] = { text = "Meter"; skip = false; state = 1; max_state = 3; options = {
+    --    [1] = {text = " Normal >";  callback = noop };
+    --    [2] = {text = "< Refill >";  callback = noop };
+    --    [3] = {text = "< Infinate";  callback = noop }
+    --}};
+    --[3] = { text = "Player 2"; skip = true };
+    --[4] = { text = "Health"; skip = false; state = 1; max_state = 3; options = {
+    --    [1] = {text = " Normal >";  callback = noop };
+    --    [2] = {text = "< Refill >";  callback = noop };
+    --    [3] = {text = "< Infinate";  callback = noop }
+    --}};
+    --[5] = { text = "Meter"; skip = false; state = 1; max_state = 3; options = {
+    --    [1] = {text = " Normal >";  callback = noop };
+    --    [2] = {text = "< Refill >";  callback = noop };
+    --    [3] = {text = "< Infinate";  callback = noop }
+    --}};
+    --[6] = { text = "State"; skip = false; state = 1; max_state = 3; options = {
+    --    [1] = {text = " Standing >";  callback = noop };
+    --    [2] = {text = "< Crouching >";  callback = noop };
+    --    [3] = {text = "< Jumping";  callback = noop }
+    --}};
+    --[7] = { text = "Blocking"; skip = false; state = 1; max_state = 3; options = {
+    --    [1] = {text = " None >";  callback = noop };
+    --    [2] = {text = "< Auto >";  callback = noop };
+    --    [3] = {text = "< Follow Up";  callback = noop }
+    --}};
+    [1] = { text = "Extras"; skip = true };
+    [2] = { text = "Time"; skip = false; state = 1; max_state = 2; options = {
         [1] = { text = " Normal >";  callback = noop };
-        [2] = { text = "< Infinate";  callback = noop }
+        [2] = { text = "< Infinate";  callback = one_byte_set_to_max({address = state.time.address; max = state.time.max}) }
     }};
-    [10] = { text = "Hitboxes"; skip = false; state = 1; max_state = 2; options = {
-        [1] = { text = " Off >";  callback = noop };
-        [2] = { text = "< On";  callback = noop }
-    }};
+    --[10] = { text = "Hitboxes"; skip = false; state = 1; max_state = 2; options = {
+    --    [1] = { text = " Off >";  callback = noop };
+    --    [2] = { text = "< On";  callback = noop }
+    --}};
 }
 
 function run_menu_callbacks()
